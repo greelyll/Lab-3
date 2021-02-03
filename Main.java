3f02e7e28b02c2c01fb2fab45d53bd6bf51ad019
greelyll <greelyll@mail.uc.edu>
2021-02-03 03:23:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter a Number To Count Down or Up From");
+   Scanner s = new Scanner(System.in);{
+  int c = 0;
+  while(c<=10)
+  {System.out.println(c);
+  c += 1; } 
+   {System.out.println("Blast Off!"); }
+  }
+
+  } }
